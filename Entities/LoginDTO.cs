a25f710bcfbe95c806e9bb7b35c0276ce1f39efb
edward colmenarez <edwardyisus9@gmail.com>
2024-02-10 19:44:51 -0400

    Login data object

---
+++ Entities/LoginDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskPro_back.Entities
+{
+    public class LoginDTO
+    {
+        [Required]
+        [EmailAddress]
+        [MaxLength(200)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Password { get; set; }
+    }
+}
