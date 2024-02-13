c23dc8af2fba57659e9c6e5d537dceda3f4d9376
edward colmenarez <edwardyisus9@gmail.com>
2024-02-13 12:30:09 -0400

    Id en comentario

--- Models/MongoModels/Comment.cs
+++ Models/MongoModels/Comment.cs
@@ -5,6 +5,10 @@ namespace TaskPro_back.Models.MongoModels
 {
     public class Comment
     {
+        [Required]
+        [BsonGuidRepresentation(MongoDB.Bson.GuidRepresentation.Standard)]
+        [BsonId]
+        public Guid Id { get; set; }
 
         [Required]
         [BsonGuidRepresentation(MongoDB.Bson.GuidRepresentation.Standard)]
@@ -13,7 +17,7 @@ namespace TaskPro_back.Models.MongoModels
         [Required]
         [BsonGuidRepresentation(MongoDB.Bson.GuidRepresentation.Standard)]
         public Guid UserId { get; set; }
-        
+
         [Required]
         public string UserName { get; set; }
         [Required]
